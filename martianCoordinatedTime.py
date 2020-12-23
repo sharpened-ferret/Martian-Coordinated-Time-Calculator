9c570fbc8a2bac11a78db6a63d195da5f8f040a6
sharpened-ferret <clonesupreme@gmail.com>
2020-12-23 23:04:16 +0000

    Finished basic MTC calculation, needs additional tweaks

--- martianCoordinatedTime.py
+++ martianCoordinatedTime.py
@@ -1,10 +1,12 @@
 import datetime
 
 # Represents the amount of seconds that Terrestrial Time is currently ahead of Universal Time
-terrestrialTimeOffset = 32.184
+terrestrialTimeOffset = 32.184 + 8
 
 def getMarsTime(currentTime):
-    print("Test")
+    solTimestamp = (getMarsDate(currentTime) * 24) % 24
+    return datetime.timedelta(0,0,0,0,0,solTimestamp)
+    return solTimestamp
 
 def getJulianDateUT(currentDate):
     dateTimeStamp = currentDate.timestamp()
@@ -20,8 +22,10 @@ def getMarsDate(currentDate):
     return marsSolDate
 
 def main():
-    testDate = datetime.datetime(2020, 12, 23, 22, 8, 27)
+    # testDate = datetime.datetime(2020, 12, 23, 22, 8, 27)
+    testDate = datetime.datetime.now()
     print(testDate.year)
-    print("Julian Date: {0}, Mars Date: {1}".format(getJulianDateUT(testDate), getMarsDate(testDate)))
-    
+    print("Julian Date: {0}, Mars Date: {1}".format(getJulianDateTT(testDate), getMarsDate(testDate)))
+    print("Current MTC: {0}".format(getMarsTime(testDate)))
+
 main()
