58788c774f105bcfa749ca2bf7dfcad38239fcb2
sharpened-ferret <clonesupreme@gmail.com>
2020-12-23 22:19:31 +0000

    Migrated to seperate Julian date functions for terrestrial and universal time

--- martianCoordinatedTime.py
+++ martianCoordinatedTime.py
@@ -6,23 +6,22 @@ terrestrialTimeOffset = 32.184
 def getMarsTime(currentTime):
     print("Test")
 
-def getJulianDate(currentDate):
+def getJulianDateUT(currentDate):
     dateTimeStamp = currentDate.timestamp()
     julianDate = (dateTimeStamp / 86400) + 2440587.5
     return julianDate
 
+def getJulianDateTT(currentDate):
+    return getJulianDateUT(currentDate) + terrestrialTimeOffset/86400
+    
 def getMarsDate(currentDate):
-    julianDate = getJulianDate(currentDate)
-    marsSolDate = (julianDate + terrestrialTimeOffset/86400 - 2405522.0025054) / 1.0274912517
+    julianDateTT = getJulianDateTT(currentDate)
+    marsSolDate = (julianDateTT - 2405522.0025054) / 1.0274912517
     return marsSolDate
 
 def main():
-    # currentDate = datetime.datetime(2020, 9, 17,0,0,0)
-    # currentTime = currentDate.time
-    # print(currentDate)
-    # currentTime
-    # print(getMarsDate())
     testDate = datetime.datetime(2020, 12, 23, 22, 8, 27)
     print(testDate.year)
-    print("Julian Date: {0}, Mars Date: {1}".format(getJulianDate(testDate), getMarsDate(testDate)))
+    print("Julian Date: {0}, Mars Date: {1}".format(getJulianDateUT(testDate), getMarsDate(testDate)))
+    
 main()
